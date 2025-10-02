652795d764c484d978f045b8360842b2a226eab7
SabbirHossainEvan <evansabbir31@gmail.com>
2025-10-02 20:38:15 +0600

    4th commit

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoredDB } from '../../utility/addToDB';
 
 const BookDetails = () => {
     const {id} = useParams();
     const bookId = parseInt(id);
     const data = useLoaderData();
     const singleBook = data.find(book => book.bookId === bookId);
-    const { bookName, image} = singleBook;
+    const { bookName, image} = singleBook || {};
+
+    const handleMarkAsRead = id => {
+        addToStoredDB(id)
+    }
 
     return (
         <div className='w-2/3 mx-auto'>
            <img className='w-48' src={image} alt="" />
            <h5>{bookName}</h5>
 
-           <button className='btn btn-accent m-2'>Read</button>
-           <button className='btn btn-info'>Wishlist</button>
+           <button onClick={handleMarkAsRead(id)} className='btn btn-accent m-2'>Mar as Read</button>
+           <button className='btn btn-info'>Add To Wishlist</button>
         </div>
     );
 };
